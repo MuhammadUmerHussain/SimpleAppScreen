a6bc793f1012fed3d02294954cb6b98b7fb96b4b
MuhammadUmerHussain <umerhussain231123@gmail.com>
2023-07-08 12:57:18 +0500

    Refactor function code into widgets

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Here Is Your Boi"),
+        title: Text("Quotes"),
         centerTitle: true,
         backgroundColor: Colors.red[900],
 
